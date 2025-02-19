88141ad77842db81981842291bf80d4e9915a291
CapitaoDAreia <danieligord7@hotmail.com>
2025-02-19 02:06:58 -0300

    chore: import.sql and properties and pom.xml

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,54 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+INSERT INTO address (id, street, number, complement, neighborhood, city, state, country, zipCode)
+VALUES (
+           UUID_TO_BIN(UUID()),
+           'Negra Arroyo Lane',
+           '308',
+           'Search for a pizza in the roof',
+           '',
+           'Albuquerque',
+           'New Mexico',
+           'United States',
+           '87104'
+       );
+
+INSERT INTO customer (id, name, surName, email, cpf, address_id)
+VALUES (
+           UUID_TO_BIN(UUID()),
+           'Walter',
+           'White',
+           'walter.white@lospolloshermanos.com',
+           '12345678900',
+           (SELECT id FROM address WHERE street = 'Negra Arroyo Lane' AND number = '308')
+       );
+
+INSERT INTO customer (id, name, surName, email, cpf, address_id)
+VALUES (
+           UUID_TO_BIN(UUID()),
+           'Skyler',
+           'White',
+           'skyler.white@benekefabricators.com',
+           '12345678999',
+           (SELECT id FROM address WHERE street = 'Negra Arroyo Lane' AND number = '308')
+       );
+
+-- Inserindo a conta para o cliente Walter White
+INSERT INTO account (id, accountNumber, agency, isActive, customer_id)
+VALUES (
+           UUID_TO_BIN(UUID()),
+           '123456',
+           '0001',
+           true,
+           (SELECT id FROM customer WHERE name = 'Walter' AND surName = 'White')
+       );
+
+INSERT INTO card (id, number, expirationDate, cvv, isActive, isBlocked, country, account_id)
+VALUES (
+           UUID_TO_BIN(UUID()),
+           '1234 5678 1234 5678',
+           '2025-12-01',
+           '123',
+           true,
+           false,
+           'United States',
+           (SELECT id FROM account WHERE accountNumber = '123456' AND agency = '0001')
+       );
